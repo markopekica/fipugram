d449e75bb3daff22078b0849ebf5003f009ba774
marko <pekica.marko@gmail.com>
2020-12-01 19:50:50 +0100

    nakon demosa, odvojena kartica u store

--- src/store.js
+++ src/store.js
@@ -1,4 +1,33 @@
 export default {
     searchTerm: '',
-    isThatTrue: '?'
+    isThatTrue: '?',
+    ime: "",
+    prezime: "",
+    cards: [
+        {
+          title: "1",
+          time: "some time",
+          url: "https://picsum.photos/id/1/400",
+          author: "Joe",
+        },
+        {
+          title: "2",
+          time: "24.11.20. 05:34",
+          url: "https://picsum.photos/id/2/400",
+          author: "Meri",
+        },
+        {
+          title: "3",
+          time: "23.11.20. 04:21",
+          url: "https://picsum.photos/id/3/400",
+          author: "Lee",
+        },
+        {
+          title: "4",
+          time: "22.11.20. 08:28",
+          url: "https://picsum.photos/id/4/400",
+          author: "Bob",
+        },
+      ]
+
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
       <div class="imeprez">
         <label for="name">Name:</label>
         <input
-          v-model="ime"
+          v-model="store.ime"
           type="text"
           id="name"
           name="name"
@@ -24,7 +24,7 @@
         <br />
         <label for="lastName">Last name:</label>
         <input
-          v-model="prezime"
+          v-model="store.prezime"
           type="text"
           id="last-name"
           name="lastName"
@@ -32,7 +32,10 @@
           size="10"
         />
       </div>
-      <div style="margin-top:1em;">{{ ime + " " + prezime }} <!-- {{ store.isThatTrue }} --></div>
+      <div style="margin-top: 1em">
+        {{ store.ime + " " + store.prezime }}
+        <!-- {{ store.isThatTrue }} -->
+      </div>
     </div>
   </div>
 </template>
@@ -41,47 +44,17 @@
 import InstagramCard from "@/components/InstagramCard.vue";
 import store from "@/store.js";
 
-let cards = [
-  {
-    title: "1",
-    time: "some time",
-    url: "https://picsum.photos/id/1/400",
-    author: "Joe",
-  },
-  {
-    title: "2",
-    time: "24.11.20. 05:34",
-    url: "https://picsum.photos/id/2/400",
-    author: "Meri",
-  },
-  {
-    title: "3",
-    time: "23.11.20. 04:21",
-    url: "https://picsum.photos/id/3/400",
-    author: "Lee",
-  },
-  {
-    title: "4",
-    time: "22.11.20. 08:28",
-    url: "https://picsum.photos/id/4/400",
-    author: "Bob",
-  },
-];
-
 export default {
   name: "home",
-  data: function () {
+  data() {
     return {
-      cards: cards,
       store,
-      ime: "",
-      prezime: "",
     };
   },
   imePrezime() {},
   computed: {
     filteredCards() {
-      return this.cards.filter(
+      return this.store.cards.filter(
         (card) =>
           card.title.includes(store.searchTerm) ||
           card.author.includes(store.searchTerm)
@@ -96,10 +69,10 @@ export default {
 
 
 <style lang="scss" scoped>
-.imeprez{
-  display:flex; 
-  flex-direction:column; 
-  align-items:center;
+.imeprez {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 1em auto;
 }
 </style>
